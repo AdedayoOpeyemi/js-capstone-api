cb0370833cd193da20a191b0ddfbc68cccfccc25
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-09-08 17:30:46 -0300

    Add modal functionality to be used  when users clicks a tvshow card

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 import './styles.css';
+import UI from './modules/UserInterface.js';
+
+UI.displayModal(Math.floor(Math.random() * (239)) + 1);
