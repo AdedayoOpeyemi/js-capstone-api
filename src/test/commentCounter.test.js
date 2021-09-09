7b488d5f76d99d30e1e6a4bd8ed2d46478699660
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-09-09 16:34:46 -0300

    Add basic sketch for testing functions

---
+++ src/test/commentCounter.test.js
@@ -0,0 +1,7 @@
+import { describe, expect } from '@jest/globals';
+
+describe('Comment Counter Testing', () => {
+  test('Counting number of items when there is only a message of no comments', () => {
+    const modalHtmlMockUp = 
+  });
+});
---
+++ src/test/modalMockUp.js
@@ -0,0 +1,8 @@
+export const modal1 = `<h3 id="modal-comments-title" class="modal-auxiliar-titles mb-3">This needs to change</h3>
+<div id="comments-container" class="d-flex flex-column mb-3"><div class="comment-item d-flex">
+       <p class="me-1"></p> 
+       <p class="me-1">No Comments Yet:</p> 
+       <p class="me-1"></p>
+     </div></div>`;
+
+export const modal2 = ``;
